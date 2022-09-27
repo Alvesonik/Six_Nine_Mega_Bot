08d6a50089ffeca0eeed78ed625e65d76af83aea
Alvesonik <lexabob.av@gmail.com>
2022-09-27 23:02:42 +0300

    add photo and emoji

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+from emoji import emojize
 from email import message
 import logging
 from urllib import request
-from random import randint
+from glob import glob
+from random import randint, choice
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -42,6 +44,12 @@ def guess_number(update, context):
         message = "Введите число"
     update.message.reply_text(message)
 
+def send_mem(update, context):
+    mem_photo_list = glob('images/mem*.jp*g')
+    mem_photo_filename = choice(mem_photo_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(mem_photo_filename, 'rb'))
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)  
     # request_kwargs=PROXY
@@ -49,6 +57,7 @@ def main():
     md = mybot.dispatcher
     md.add_handler(CommandHandler('start', greet_user))
     md.add_handler(CommandHandler('guess', guess_number))
+    md.add_handler(CommandHandler('photo', send_mem))
     md.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Bot starting")
